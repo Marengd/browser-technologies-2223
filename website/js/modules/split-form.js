d4bb75429aca8b427747b836ff8eadda46ff1583
BuildTools <unconfigured@null.spigotmc.org>
2023-03-30 22:51:01 +0200

    Added Interactive Step-Indicator

--- website/js/modules/split-form.js
+++ website/js/modules/split-form.js
@@ -1,6 +1,18 @@
 // split-form.js
 import StepIndicator from './step-indicator.js';
 
+function showFieldset(fieldsets, index) {
+  fieldsets.forEach((fs, idx) => {
+    if (idx === index) {
+      fs.classList.remove('hidden');
+      fs.classList.add('visible');
+    } else {
+      fs.classList.remove('visible');
+      fs.classList.add('hidden');
+    }
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   const fieldsets = document.querySelectorAll('form fieldset');
   const stepIndicatorElement = document.querySelector('ol');
@@ -33,19 +45,25 @@ document.addEventListener('DOMContentLoaded', function () {
           currentFieldsetIndex--;
         }
 
-        fieldsets.forEach((fs, idx) => {
-          if (idx === currentFieldsetIndex) {
-            fs.classList.remove('hidden');
-            fs.classList.add('visible');
-          } else {
-            fs.classList.remove('visible');
-            fs.classList.add('hidden');
-          }
-        });
-
-        // Update the step indicator when the fieldset changes
+        showFieldset(fieldsets, currentFieldsetIndex);
         stepIndicator.updateCurrentStep(currentFieldsetIndex);
       });
     });
+  });
+
+  // Update the step indicator when the anchor in the step indicator is clicked
+  stepIndicator.olElement.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.tagName === 'A') {
+      const fieldsetId = target.getAttribute('href').substr(1);
+      const fieldsetIndex = Array.from(fieldsets).findIndex(
+        (fs) => fs.id === fieldsetId
+      );
+      if (fieldsetIndex > -1) {
+        currentFieldsetIndex = fieldsetIndex;
+        showFieldset(fieldsets, currentFieldsetIndex);
+        stepIndicator.updateCurrentStep(currentFieldsetIndex);
+      }
+    }
   });
 });
--- website/js/modules/step-indicator.js
+++ website/js/modules/step-indicator.js
@@ -1,19 +1,29 @@
 class StepIndicator {
-   constructor(olElement) {
-     this.olElement = olElement;
-     this.steps = olElement.querySelectorAll('li');
-   }
- 
-   updateCurrentStep(index) {
-     this.steps.forEach((step, idx) => {
-       if (idx === index) {
-         step.classList.add('current');
-       } else {
-         step.classList.remove('current');
-       }
-     });
-   }
- }
+  constructor(olElement) {
+    this.olElement = olElement;
+    this.steps = olElement.querySelectorAll('li');
+    this.initEvents();
+  }
+
+  initEvents() {
+    this.steps.forEach((step, index) => {
+      step.querySelector('a').addEventListener('click', (event) => {
+        event.preventDefault();
+        this.updateCurrentStep(index);
+      });
+    });
+  }
+
+  updateCurrentStep(index) {
+    this.steps.forEach((step, idx) => {
+      if (idx === index) {
+        step.classList.add('current');
+      } else {
+        step.classList.remove('current');
+      }
+    });
+  }
+}
  
  export default StepIndicator;
  
