38f69fd6a0ceb42abd181d5b6bfc25b987b4acda
marengd <danian.marengo@hva.nl>
2023-03-30 17:50:20 +0200

    Added Intermediate HTML Structure and CSS Styling

--- website/js/modules/form-button-handler.js
+++ website/js/modules/form-button-handler.js
@@ -1,28 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const fieldsets = document.querySelectorAll('form fieldset');
-    for (let i = 0; i < fieldsets.length; i++) {
-       const fieldset = fieldsets[i];
+   const fieldsets = document.querySelectorAll('form fieldset');
+   for (let i = 0; i < fieldsets.length; i++) {
+     const fieldset = fieldsets[i];
+     const nav = document.createElement('nav'); // Create a new nav element
  
-       // Check if the fieldset contains a submit button
-       const hasSubmitButton = fieldset.querySelector('button[type="submit"]');
+     // Check if the fieldset contains a submit button
+     const hasSubmitButton = fieldset.querySelector('button[type="submit"]');
  
-       if (!hasSubmitButton) {
-          if (i > 0) {
-             const prevButton = document.createElement('button');
-             prevButton.type = 'button';
-             prevButton.textContent = 'previous';
-             prevButton.setAttribute('aria-label', 'previous');
-             fieldset.appendChild(prevButton);
-          }
-    
-          if (i < fieldsets.length - 1) {
-             const nextButton = document.createElement('button');
-             nextButton.type = 'button';
-             nextButton.textContent = i === 0 ? 'Volgende' : 'next';
-             nextButton.setAttribute('aria-label', 'next');
-             fieldset.appendChild(nextButton);
-          }
+     if (!hasSubmitButton) {
+       if (i > 0) {
+         const prevButton = document.createElement('button');
+         prevButton.type = 'button';
+         prevButton.textContent = 'previous';
+         prevButton.setAttribute('aria-label', 'previous');
+         nav.appendChild(prevButton); // Append the button to the nav element
        }
-    }
+ 
+       if (i < fieldsets.length - 1) {
+         const nextButton = document.createElement('button');
+         nextButton.type = 'button';
+         nextButton.textContent = i === 0 ? 'Volgende' : 'next';
+         nextButton.setAttribute('aria-label', 'next');
+         nav.appendChild(nextButton); // Append the button to the nav element
+       }
+ 
+       fieldset.appendChild(nav); // Append the nav element to the fieldset
+     }
+   }
  });
  
