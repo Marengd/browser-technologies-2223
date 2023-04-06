e7f4ad8bcae45575dcec6cbbc6b38be9d6daf72f
marengd <danian.marengo@hva.nl>
2023-04-06 13:57:36 +0200

    ...

--- website/js/modules/browserCorrections.js
+++ website/js/modules/browserCorrections.js
@@ -1,26 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-   // Get all radio inputs with the specified conditions
-   var radioInputs = document.querySelectorAll('input[type="radio"]');
- 
-   function updateCheckedLabel() {
-     radioInputs.forEach(function(input) {
-       var label = input.closest("label");
-       if (input.checked) {
-         label.classList.add("checked-label");
-       } else {
-         label.classList.remove("checked-label");
-       }
-     });
-   }
- 
-   // Initialize the checked labels
-   updateCheckedLabel();
- 
-   // Attach a change event listener to update the checked-label class
-   document.addEventListener("change", function(event) {
-     if (event.target.matches('input[type="radio"]')) {
-       updateCheckedLabel();
-     }
-   });
- });
- 
+  // Get all radio inputs with the specified conditions
+  var radioInputs = document.querySelectorAll('input[type="radio"][id^="rating-ul"], input[type="radio"][id^="rating-tm"], input[type="radio"][id^="rating-ex"]');
+
+  function updateCheckedLabel() {
+    radioInputs.forEach(function(input) {
+      var label = input.closest("label");
+      if (input.checked) {
+        label.classList.add("checked-label");
+      } else {
+        label.classList.remove("checked-label");
+      }
+    });
+  }
+
+  // Initialize the checked labels
+  updateCheckedLabel();
+
+  // Attach a change event listener to update the checked-label class
+  document.addEventListener("change", function(event) {
+    if (event.target.matches('input[type="radio"][id^="rating-ul"], input[type="radio"][id^="rating-tm"], input[type="radio"][id^="rating-ex"]')) {
+      updateCheckedLabel();
+    }
+  });
+});
